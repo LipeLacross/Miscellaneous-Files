0b26756c28d9bb186c1276947bc65960d153e706
Felipe Moreira <feliperioscross.br@gmail.com>
2022-12-02 23:15:08 -0300

    Função encontrar

---
+++ Vs code Java/A. E. D. ENCONTRAR/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8};
+        int number = 4;
+        
+        System.out.println(finding.find(number, numbers));
+    }
+}
---
+++ Vs code Java/A. E. D. ENCONTRAR/src/finding.java
@@ -0,0 +1,13 @@
+public class finding {
+    public static boolean find(int item, int[] lista)
+        {
+            for (int i = 0; i < lista.length; i++)
+            {
+                if (lista[i] == item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+}
