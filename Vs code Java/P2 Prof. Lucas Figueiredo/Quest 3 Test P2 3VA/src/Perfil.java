6806aeac5af2370d8c85539437a7e355d158e75d
Felipe <feliperioscross.br@gmail.com>
2023-02-26 16:30:37 -0300

    Transfer and addition

--- Vs code Java/P2 Prof. Lucas Figueiredo/Quest 3 Test P2 3VA/src/Perfil.java
+++
@@ -1,9 +0,0 @@
-public class Perfil {
-
-    String name, arroba;
-    Perfil(String name, String arroba)
-    {
-        this.name = name;
-        this.arroba = arroba;
-    }
-}
--- Vs code Java/P2 Prof. Lucas Figueiredo/Quest 3 Test P2 3VA/src/Profile.java
+++
@@ -1,8 +0,0 @@
-public class Profile {
-    
-    String name, arroba;
-    Profile()
-    {
-        
-    }
-}
